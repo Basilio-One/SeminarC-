b31f457412f864662c151931debe838c2a742b5b
Basilio-one <vasya-w@yandex.ru>
2022-11-27 05:42:05 +0300

    zadacha 2

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число для выяснения дня недели");
+int Day = Convert.ToInt32(ConsolReadLine());
+string dayweek = Console.ReadLine("F");
+if (Day == 1) dayweek = ("Понедельник");
+if (Day == 2) dayweek = ("Вторник");
+if (Day == 3) dayweek = ("Среда");
+if (Day == 4) dayweek = ("Четверг");
+if (Day == 5) dayweek = ("Пятница");
+if (Day == 6) dayweek = ("Суббота");
+if (Day == 7) dayweek = ("Воскресение");
+
+Console.WriteLine("dayweek= ");
+Console.WriteLine("dayweek");
