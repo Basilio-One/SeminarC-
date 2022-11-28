3366029c248bcd847b098e999fc64cfa616ff35f
Basilio-one <vasya-w@yandex.ru>
2022-11-29 00:45:03 +0300

    Exemple010 metod poiska array

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1, 22, 3, 5, 4, 9, 8, 22 };
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
