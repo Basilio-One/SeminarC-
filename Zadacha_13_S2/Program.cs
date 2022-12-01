58a1629f90b89757c55e9fcdfd21a35f9e708075
Basilio-one <vasya-w@yandex.ru>
2022-12-01 23:34:27 +0300

    Zadacha na seminare-2-3

---
+++ Zadacha_13_S2/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random(). Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное трехзначное число {num}" );
+int x1 = num/100;
+int x2 =  num%10; // остаток деления
+//Console.WriteLine($"{num} -> {x1}{x2}");
+Console.Write(x1);
+Console.Write(x2);
