d5466cc34beca421a8872f54a23c9175eac25288
Basilio-one <vasya-w@yandex.ru>
2022-12-01 22:26:32 +0300

    Zadacha na seminare-2-1

---
+++ Zadacha_12_S2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 =num1%10;
+if (num2*num2 == num1)
+{
+    Console.Write ($"{num2}, {num1} -> Кратно");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} -> Не кратно, остаток {num3} ");
+}
