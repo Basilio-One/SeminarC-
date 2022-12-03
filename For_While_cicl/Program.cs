94d76cd6a3bf1821c964a7cf7201d191c1682fb9
Basilio-one <vasya-w@yandex.ru>
2022-12-03 15:49:40 +0300

    Cicl for tablicha umnojrnia

---
+++ For_While_cicl/Program.cs
@@ -0,0 +1,20 @@
+﻿// string Method4(int count,string c)
+// {
+//     string result = String.Empty; // String.Empty или "" - пустая строка
+//     for (int i=0; i<count; i++)
+//     {
+//         result =result + c;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "ZK");
+// Console.WriteLine(res); // результак который метод будет возвращать
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+Console.WriteLine();
+}
